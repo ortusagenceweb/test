0e85a05cb4d145ac330575d6dbe5674231190159
ortusagenceweb <contact@ortusagenceweb.fr>
2017-04-04 15:48:09 +0200

    Modifié le fichier index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo 'Un fichier index bidon en php pour envoyer sur le repo de test en CLI';
+
+echo '<br />Ajout d\'une ligne supplémentaire';
+
+echo '<p>Encore une autre ligne dans un paragraphe</p>';
+
+?>
